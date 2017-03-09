8649fa554ecaa9086cbd2180069ed7b6f3022d57
zhaoli <seseva57@gmail.com>
2017-03-09 16:13:53 +0800

    config qiniu

--- config/routes.rb
+++ config/routes.rb
@@ -28,13 +28,13 @@ Rails.application.routes.draw do
 
   resources :cart_items
 
-   resources :orders do
-     member do
-       post :pay_with_alipay
-       post :pay_with_wechat
-       post :apply_to_cancel
-     end
-   end
+  resources :orders do
+    member do
+      post :pay_with_alipay
+      post :pay_with_wechat
+      post :apply_to_cancel
+    end
+  end
 
   namespace :account do
     resources :orders
